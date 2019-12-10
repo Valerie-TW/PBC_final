ce46e14bc0b447a6c4a45d55f259939496b23dd1
kiwiFurry <52002072+kiwiFurry@users.noreply.github.com>
2019-12-11 00:51:22 +0800

    demo simple URL GET requests

---
+++ app/controller/__init__.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+blueprint = Blueprint('view',__name__)
+from . import routes
--- entry.py
+++ entry.py
@@ -1,9 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from app import initApp, socketio
+# import eventlet
 
-@app.route("/")
-def hello():
-    return "<h1>Hello World!</h1>"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app = initApp(debug=True)
+    host='0.0.0.0'
+    # port = 5000 + random.randint(0, 999)
+    port = 5000 #debug mode
+    # url = "http://{}:{}".format(host,port)  #ID-a test
+    # browser = subprocess.Popen(['firefox', url])
+    # webbrowser.open(url)
+    
+    socketio.run(app,host=host,port=port)
